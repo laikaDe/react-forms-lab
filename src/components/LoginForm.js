ca2377e02a377279116457911f772726d6ab84fe
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-05-02 17:12:16 -0700

    commit tree db5e66143767d9a152061077ebb3cde9631b92f6

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,63 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const username = this.state.username
+    const password = this.state.password
+    if (username !== "" && password !== "") {
+      this.props.handleLogin({username, password})
+      this.setState({
+        username: '',
+        password: ''
+      });
+    }
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+
 
   render() {
     return (
-      <form>
+      <form
+      onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            value={this.state.username}
+            onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password"
+            value={this.state.password}
+            onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onChange={this.onSubmit}>Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,23 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      message: ''
+    };
+  }
 
-    this.state = {};
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" value={this.state.message} onChange={this.handleInputChange} name="message" id="message" />
+        <p>You have got {this.props.maxChars - this.state.message.length} characters remaining. </p>
       </div>
     );
   }
